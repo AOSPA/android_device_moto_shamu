f1c0db5f449f27221ea79969a2fb26eadc91f348
dhacker29 <dhackerdvm@gmail.com>
2015-06-03 01:39:18 -0400

    Disable dexpreopt only on affected apk's
    Change-Id: I6810200151245e25eea9d3f764bbc8b50e7388e9
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- device.mk
+++ device.mk
@@ -222,6 +222,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.no_wait_for_card=1
 
+# never dexopt the MotoSignature
+$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
+
 #Reduce IMS logging
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.ims.disableDebugLogs=1
