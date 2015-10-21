ea821293ea5e24bb30de8bd1ecbcfb630e76313e
Oliver Reiche <oliver.reiche@gmail.com>
2015-10-21 21:00:55 +0200

    shamu: Initial PA marshmallow bringup

--- full_shamu.mk
+++ full_shamu.mk
@@ -23,18 +23,13 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_shamu
+PRODUCT_NAME := full_shamu
 PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Shamu
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, device/moto/shamu/device.mk)
-$(call inherit-product-if-exists, vendor/moto/shamu/device-vendor.mk)
-
-PRODUCT_NAME := aosp_shamu
-
-PRODUCT_PACKAGES += \
-    Launcher3
+$(call inherit-product-if-exists, vendor/moto/shamu/shamu-vendor.mk)
 
