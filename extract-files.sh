f08315e22d33371eeb3d6d8ce7e1f042bd4bc390
Evan Anderson <evananderson@aospa.co>
2016-02-19 19:52:05 -0500

    proprietaries: Update list and extract script
    Using extract script from CM angler to rebuild jars and apks
    
    Change-Id: Ibb61b530b30da2ad0c2a120cc52c328175bd87ec
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- extract-files.sh
+++ extract-files.sh
@@ -1,62 +1,86 @@
-#!/bin/bash
+#!/bin/bash -e
 
 export VENDOR=moto
 export DEVICE_VENDOR=moto
 export DEVICE=shamu
+export PROPRIETARY_FILES=device-proprietary-files.txt
 
-# Check to see if the user passed a folder in to extract from rather than adb pull
-if [ $# -eq 1 ]; then
-    COPY_FROM=$1
-    test ! -d "$COPY_FROM" && echo error reading dir "$COPY_FROM" && exit 1
+TMPDIR="/tmp/extractfiles.$$"
+mkdir "$TMPDIR"
+
+# relative to SMALIBASE defined by 2nd cli arg to this script
+SMALIJAR=smali/build/libs/smali.jar
+BAKSMALIJAR=baksmali/build/libs/baksmali.jar
+
+# Only supports extract from filesystem
+
+if [[ "$#" -ne 2  || ! -f "$2/$SMALIJAR" || ! -f "$2/$BAKSMALIJAR" ]]; then
+    echo "Usage: $0 <path to root dir of extracted filesystem> <smali base dir for git clone https://github.com/JesusFreke/smali> >"
+    echo "  root dir must contain at least system directions"
+    echo "  smali base must contain built jar objects (within smali base run: ./gradew build)"
+    exit 1
 fi
 
-set -e
+COPY_FROM="$1"
+SMALIBASE="$2"
+
+function oat2dex()
+{
+    OFILE="$1"
+
+    OAT="`dirname $OFILE`/oat/arm/`basename $OFILE ."${OFILE##*.}"`.odex"
+    if [ ! -e "$OAT" ]; then
+        return 0
+    fi
+
+    java -jar "$SMALIBASE/$BAKSMALIJAR" -x -o "$TMPDIR/dexout" -c boot.oat -d "$COPY_FROM/framework/arm" "$OAT"
+    java -jar "$SMALIBASE/$SMALIJAR" "$TMPDIR/dexout" -o "$TMPDIR/classes.dex"
+    rm -rf "$TMPDIR/dexout"
+}
 
 function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-        echo "Extracting /system/$FILE ..."
+    OUTBASE="$2"
+    for FILE in $(egrep -v '(^#|^$)' $1); do
+        echo "Extracting $FILE ..."
         OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-        DEST=${PARSING_ARRAY[1]}
-        if [ -z $DEST ]; then
-            DEST=$FILE
+        SRCFILE=$(echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g")
+        DESTFILE=${PARSING_ARRAY[1]}
+        if [ -z "$DESTFILE" ]; then
+            DESTFILE="$SRCFILE"
         fi
-        DIR=`dirname $FILE`
-        if [ ! -d $2/$DIR ]; then
-            mkdir -p $2/$DIR
+        DESTFILE=$(echo "$DESTFILE" | sed 's|^system/||')
+        DESTDIR=$(dirname "$DESTFILE")
+        if [ ! -d "$OUTBASE/$DESTDIR" ]; then
+            mkdir -p "$OUTBASE/$DESTDIR"
         fi
-        if [ "$COPY_FROM" = "" ]; then
-            # Try destination target first
-            if [ -f /system/$DEST ]; then
-                adb pull /system/$DEST $2/$DEST
-            else
-                # if file does not exist try OEM target
-                if [ "$?" != "0" ]; then
-                    adb pull /system/$FILE $2/$DEST
-                fi
-            fi
-        else
-            # Try destination target first
-            if [ -f $COPY_FROM/$DEST ]; then
-                cp $COPY_FROM/$DEST $2/$DEST
-            else
-                # if file does not exist try OEM target
-                if [ "$?" != "0" ]; then
-                    DIR=`dirname $DEST`
-                    if [ ! -d $2/$DIR ]; then
-                        mkdir -p $2/$DIR
-                    fi
-                    cp $COPY_FROM/$FILE $2/$DEST
-                fi
+
+        cp "$COPY_FROM/$SRCFILE" "$OUTBASE/$DESTFILE"
+
+        # Fixup xml files
+        if [[ "$OUTBASE/$DESTFILE" =~ .xml$ ]]; then
+            xmlheader=$(grep '^<?xml version' "$OUTBASE/$DESTFILE")
+            grep -v '^<?xml version' "$OUTBASE/$DESTFILE" > "$OUTBASE/$DESTFILE".temp
+            (echo "$xmlheader"; cat "$OUTBASE/$DESTFILE".temp ) > "$OUTBASE/$DESTFILE"
+            rm "$OUTBASE/$DESTFILE".temp
+        fi
+        if [[ "$DESTFILE" =~ .(apk|jar)$ ]]; then
+            oat2dex "$COPY_FROM/$SRCFILE"
+            if [ -e "$TMPDIR/classes.dex" ]; then
+                zip -gjq "$OUTBASE/$DESTFILE" "$TMPDIR/classes.dex"
+                rm "$TMPDIR/classes.dex"
+                echo "Updated $OUTBASE/$DESTFILE from odex files"
             fi
         fi
     done
 }
 
-DEVICE_BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVICE_BASE/*
+DEVICE_BASE="../../../vendor/$VENDOR/$DEVICE/proprietary"
+rm -rf "$DEVICE_BASE"/*
 
 # Extract the device specific files
-extract ../../$DEVICE_VENDOR/$DEVICE/device-proprietary-files.txt $DEVICE_BASE
+extract "../../$DEVICE_VENDOR/$DEVICE/$PROPRIETARY_FILES" "$DEVICE_BASE"
+
+# clean temp dir
+rm -rf "$TMPDIR"
 
 ./setup-makefiles.sh
